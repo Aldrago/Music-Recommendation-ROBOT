2491f6f2993046600349ff86825d651c13cd24ad
Aldrago <40691901+Aldrago@users.noreply.github.com>
2020-08-20 18:21:40 +0530

    Update Nt.py

--- robot/Nt.py
+++ robot/Nt.py
@@ -1,6 +1,7 @@
 import pandas
 from sklearn.model_selection import train_test_split
 import MyRecommender as Recommenders
+#This is made by Aldrago
 import numpy
 def train(userNumber):
     triplets_file='User.csv'
